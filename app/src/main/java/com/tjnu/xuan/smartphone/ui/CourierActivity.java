c421fd04a2de405defa14dd93bb889284bc6590c
993113161 <993113161@qq.com>
2017-06-19 18:01:18 +0800

    图像裁剪，拍照

---
+++ app/src/main/java/com/tjnu/xuan/smartphone/ui/CourierActivity.java
@@ -0,0 +1,12 @@
+package com.tjnu.xuan.smartphone.ui;
+
+/*
+ *  项目名：  com.tjnu.xuan.smartphone.ui
+ *  包名：    SmartPhone
+ *  文件名:   CourierActivity
+ *  创建者:   happy_yuxuan
+ *  创建时间:  17/6/19 下午5:54
+ *  描述：    useto
+ */
+public class CourierActivity {
+}
