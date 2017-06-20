e5afda373bb74ff59b3f4233df6d61bcc85f3b0e
993113161 <993113161@qq.com>
2017-06-20 17:28:47 +0800

    定稿

--- app/src/main/java/com/tjnu/xuan/smartphone/ui/SettingActivity.java
+++ app/src/main/java/com/tjnu/xuan/smartphone/ui/SettingActivity.java
@@ -1,17 +1,45 @@
 package com.tjnu.xuan.smartphone.ui;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.tjnu.xuan.smartphone.R;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SettingActivity extends BaseActivity  {
-
-
+    private ListView mListView;
+    private List<String> mList = new ArrayList<>();
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        //去除阴影
+        getSupportActionBar().setElevation(0);
+
+        initView();
     }
+
+    //初始化View
+    private void initView() {
+        mListView = (ListView) findViewById(R.id.mListView);
+
+        mList.add(getString(R.string.app_name));
+        mList.add(getString(R.string.text_app_name));
+        mList.add(getString(R.string.text_class));
+        mList.add(getString(R.string.text_person));
+        mList.add(getString(R.string.text_website_address));
+
+        mAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mList);
+        //设置适配器
+        mListView.setAdapter(mAdapter);
+    }
+
+
 
 }
